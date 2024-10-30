78367e269201b7045324abf0e7101c26099a22bb
wongakim-99 <202021000@sangmyung.kr>
2024-10-30 10:33:47 +0900

    스프링부트 초기세팅

---
+++ swaggertest/src/test/java/org/zeorck/swaggertest/SwaggertestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zeorck.swaggertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
